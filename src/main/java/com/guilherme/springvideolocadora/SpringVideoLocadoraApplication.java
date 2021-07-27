658406f55f0e97e90a179775c820e6774643310b
Guilherme C. Tonello <guitonello@outlook.com.br>
2021-07-27 11:48:10 -0300

    Commit inicial

---
+++ src/main/java/com/guilherme/springvideolocadora/SpringVideoLocadoraApplication.java
@@ -0,0 +1,13 @@
+package com.guilherme.springvideolocadora;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringVideoLocadoraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringVideoLocadoraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/guilherme/springvideolocadora/SpringVideoLocadoraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guilherme.springvideolocadora;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringVideoLocadoraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
