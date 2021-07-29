479b2dff2e542fe69501df7113c67eddeb802062
Guilherme C. Tonello <guitonello@outlook.com.br>
2021-07-29 13:45:39 -0300

    Melhorando pesquisa por nome

--- src/main/java/com/guilherme/springvideolocadora/repository/CategoriaRepository.java
+++ src/main/java/com/guilherme/springvideolocadora/repository/CategoriaRepository.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 
 import com.guilherme.springvideolocadora.model.Categoria;
 
@@ -12,6 +13,7 @@ public interface CategoriaRepository extends MongoRepository<Categoria, String> 
 	
 	Optional<Categoria> findByNome(String nome);
 	
+	@Query(value = "{ 'nome': {$regex: /.*?0.*/, $options: 'i'} }")
 	Page<Categoria> findByNomeIgnoreCaseContaining(String nome, Pageable pageable);
 	
 }
